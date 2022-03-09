74e896a2c8870ffb6df990345c1cd0f6437fda90
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-03-09 00:01:38 -0300

    CRUD realizado, cors activado para cualquier direccion

---
+++ backend-tareas/Context/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using backend_tareas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_tareas.Context
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
---
+++ backend-tareas/Models/Tarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_tareas.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Nombre { get; set; }
+
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
